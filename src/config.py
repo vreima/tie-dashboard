2d422e5dd7dd420d5e3db9feaf31691b75185a55
Ville <ville.reima@gmail.com>
2023-07-12 09:53:33 +0300

    config

--- src/config.py
+++ src/config.py
@@ -2,19 +2,24 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    # mongohost: str
-    # mongopassword: str
-    # mongoport: str
+    #mongohost: str
+    #mongopassword: str
+    #mongoport: str
     mongo_url: str
     severa_client_id: str
     severa_client_secret: str
     severa_client_scope: str
+    severa_base_url: str
     slack_token_bot: str
     openai_api_key: str
     openai_api_org: str
     admin_password: str
     railway_static_url: str
     debug_mode: bool = False
+    admin_password:str
+    channel_yks_tietomallintaminen: str
+    channel_tie_tarjouspyynnot: str
+    channel_tie_testaus: str
 
 
 settings = Settings()
